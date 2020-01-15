4cb9565a24f27ecc8b3278e1ee10224a490d591d
gao <981359741@qq.com>
2020-01-15 21:00:42 +0800

    频道编辑-删除频道及切换频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -4,20 +4,31 @@
   <div class="channel-edit">
     <!-- :border="false"  是否显示边框 -->
     <van-cell title="我的频道" :border="false">
-        <!--type="danger"背景红色  -->
-      <van-button size="mini" round type="danger">编辑</van-button>
+      <!--type="danger"背景红色  -->
+      <!-- 我的频道 -->
+      <van-button size="mini"
+       round type="danger"
+       @click="isEditShow = !isEditShow">
+          {{ isEditShow ? '完成' : '编辑'}}
+          </van-button>
     </van-cell>
     <van-grid :gutter="10" clickable>
-      <van-grid-item v-for="channel in userChannels"
-      :key="channel.id"
-      :text="channel.name" />
-    </van-grid>
-    <van-cell class="channel-header" title="推荐频道" :border="false" />
-   <van-grid :gutter="10" clickable>
-      <van-grid-item v-for="channel in remainingChannels"
+      <van-grid-item v-for="(channel,index) in userChannels"
       :key="channel.id"
       :text="channel.name"
-      @click="Addchannel(channel)"
+      @click="onUserChannelClick(index)"
+      >
+        <van-icon slot="icon" name="close" v-show="isEditShow&&!index==0" />
+      </van-grid-item>
+    </van-grid>
+    <!-- 推荐频道 -->
+    <van-cell class="channel-header" title="推荐频道" :border="false" />
+    <van-grid :gutter="10" clickable>
+      <van-grid-item
+        v-for="channel in remainingChannels"
+        :key="channel.id"
+        :text="channel.name"
+        @click="Addchannel(channel)"
       />
     </van-grid>
   </div>
@@ -40,13 +51,14 @@ export default {
   },
   data () {
     return {
-      allChannels: []// 所有频道
+      allChannels: [], // 所有频道
+      isEditShow: false// 默认叉号隐藏   编辑状态
     }
   },
   computed: {
     // 剩余的频道
     remainingChannels () {
-      const channels = []// 定义剩余的频道
+      const channels = [] // 定义剩余的频道
       const { allChannels, userChannels } = this
       allChannels.forEach(item => {
         // 当前的遍历项是否属于我的频道，如果不是，那就收集到 channels
@@ -71,6 +83,16 @@ export default {
     // 编辑添加频道
     Addchannel (channel) {
       this.userChannels.push(channel)
+    },
+    onUserChannelClick (index) {
+      // 如果是编辑状态，且下标不等于0，则执行删除操作
+      if (this.isEditShow && index !== 0) {
+        // splice  从所引处开始(包括索引本身)，删除指定的个数
+        this.userChannels.splice(index, 1)
+        // 如果是非编辑状态，则执行切换频道操作
+      } else {
+        this.$emit('switch', index)// switch切换
+      }
     }
   }
 }
@@ -86,6 +108,12 @@ export default {
   /deep/ .van-grid-item {
     width: 80px;
     height: 43px;
+    position: relative;
+     .van-grid-item__icon-wrapper {
+      position: absolute;
+      top: -1px;
+      right: 0px;
+    }
     .van-grid-item__content {
       background: #f4f5f6;
     }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,31 +1,21 @@
 <template>
   <div class="home-container">
- <!-- 最上面的登录 -->
+    <!-- 最上面的登录 -->
     <van-nav-bar title="首页" fixed></van-nav-bar>
 
     <!-- 频道列表
     v-model="active"控制激活的标签
     title标签标题
     -->
-<van-tabs v-model="active">
-    <!-- 通过插槽把面包按钮放进去 -->
-      <van-icon
-        class="wap-nav"
-        slot="nav-right"
-        name="wap-nav"
-        @click="isChannelEditShow = true"
-      />
-  <van-tab :title="channel.name"
-  v-for="channel in userChannels"
-  :key="channel.id"
-  >
-    <!-- 文章列表组件 -->
-    <article-list
-    :channel='channel'
-    ></article-list>
-  </van-tab>
-</van-tabs>
-  <!-- 编辑频道 -->
+    <van-tabs v-model="active">
+      <!-- 通过插槽把面包按钮放进去 -->
+      <van-icon class="wap-nav" slot="nav-right" name="wap-nav" @click="isChannelEditShow = true" />
+      <van-tab :title="channel.name" v-for="channel in userChannels" :key="channel.id">
+        <!-- 文章列表组件 -->
+        <article-list :channel="channel"></article-list>
+      </van-tab>
+    </van-tabs>
+    <!-- 编辑频道 -->
     <van-popup
       v-model="isChannelEditShow"
       position="bottom"
@@ -34,12 +24,14 @@
       closeable
       close-icon-position="top-left"
     >
-    <!-- 放入频道弹层组件
+      <!-- 放入频道弹层组件
     :user-channel="userChannels"父传子
-    -->
-    <channel-edit :user-channels="userChannels"></channel-edit>
+      -->
+      <channel-edit
+      :user-channels="userChannels"
+      @switch="onChannelSwitch">
+      </channel-edit>
     </van-popup>
-
   </div>
 </template>
 
@@ -56,7 +48,7 @@ export default {
     return {
       active: 0, // 控制激活的标签页
       userChannels: [], // 用户频道列表
-      isChannelEditShow: false// 弹层默认不显示
+      isChannelEditShow: false // 弹层默认不显示
     }
   },
 
@@ -74,6 +66,10 @@ export default {
         console.log(err)
         this.$toast('获取频道数据失败')
       }
+    },
+    onChannelSwitch (index) {
+      this.active = index// 切换激活频道
+      this.isChannelEditShow = false// 关闭弹层
     }
   }
 }
@@ -90,7 +86,7 @@ export default {
   line-height: 44px;
   background: #fff;
   font-size: 30px;
-  opacity: .9;
+  opacity: 0.9;
 }
 /deep/ .van-tabs__wrap {
   position: fixed;
